70b1cb213d0457234054759773416e0ab18093d1
DomiTP <domi.tarrazona@gmail.com>
2021-11-18 09:34:51 +0100

    Actividad 5 Corregida

--- Unidad 2/Actividades/Activitat 5.py
+++ Unidad 2/Actividades/Activitat 5.py
@@ -1,19 +1,25 @@
-import random
 import sys
+import random
 
+from PySide6.QtCore import Signal
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 class MainWindow(QMainWindow):
+    num_random = Signal(bool, int)
     def __init__(self):
         super().__init__()
+        self.num_random.connect(self.imprimir)
         btn = QPushButton("Press me")
         btn.setCheckable(True)
-        btn.clicked.connect(lambda checked: self.button_clicked(checked))
+        btn.clicked.connect(self.button_clicked)
         self.setCentralWidget(btn)
-
+        
     def button_clicked(self, checked):
-        print(checked, random.randint(0, 10))
+        self.num_random.emit(checked, random.randint(0, 10))
+        
+    def imprimir(self, checked, random):
+        print(checked, random)
 
 
 app = QApplication(sys.argv)
